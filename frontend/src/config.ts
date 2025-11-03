b1bb04f318dc712887f170d26a4f82ae5808fab6
Sujal Taneja <sujaltaneja@outlook.com>
2025-11-03 10:25:40 +0530

    deployment commit

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,4 @@
-export const BACKEND_URL = 'https://second-brain-knd9.onrender.com';
-// export const BACKEND_URL = 'http://localhost:3000';
-export const FRONTEND_URL = 'https://second-brain-suzie.vercel.app';
-// export const FRONTEND_URL = 'http://localhost:5173';
+export const BACKEND_URL = "https://second-brain-backend-njfy.onrender.com";
+// export const BACKEND_URL = "http://localhost:3000";
+export const FRONTEND_URL = "https://second-brain-suzie.vercel.app";
+// export const FRONTEND_URL = "http://localhost:5173";
